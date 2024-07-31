8be7838a390a60bbf0173b50c7e8a955f51f8079
mopeneko <mopeneko@lem0n.cc>
2024-08-01 07:20:33 +0900

    Use default look and feel

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -11,6 +11,66 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
+      general = {
+        gaps_in = 5;
+        gaps_out = 20;
+
+        border_size = 2;
+
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        resize_on_border = false;
+
+        allow_tearing = false;
+
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 10;
+
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+
+          vibrancy = 0.1696;
+        };
+      };
+
+      animations = {
+        enabled = true;
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = true; # You probably want this
+      };
+
+      master = {
+        new_status = "master";
+      };
+
       "$mainMod" = "SUPER";
       "$terminal" = "alacritty";
       "$menu" = "wofi --show drun";
@@ -47,6 +107,9 @@
 
       exec-once = "fcitx5";
       env = [
+        "XCURSOR_SIZE, 24"
+	"HYPRCURSOR_SIZE, 24"
+      ] ++ [
         "GTK_IM_MODULE, fcitx"
         "QT_IM_MODULE, fcitx"
         "XMODIFIER, @im=fcitx"
