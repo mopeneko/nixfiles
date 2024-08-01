d607a8d116e73e3b9390488dc2ddf16b9bda7e07
mopeneko <mopeneko@lem0n.cc>
2024-08-01 21:43:58 +0900

    Allow unfree packages on home manager

--- flake.nix
+++ flake.nix
@@ -37,7 +37,10 @@
 
       homeConfigurations = {
         "user@desktop" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
           extraSpecialArgs = {
             inherit inputs outputs;
           };
